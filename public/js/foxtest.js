84793bd96758ff293f7daf8d98e6f3bd64284e1f
Bruno Teixeira <bteixeira666@gmail.com>
2014-10-07 20:46:36 +0200

    Clean up and document.

--- public/js/foxtest.js
+++ public/js/foxtest.js
@@ -1,126 +1,39 @@
-$(function () {
-return;
-    var mapOptions = {
-        center: {
-            lat: 52.51,
-            lng: 13.37
-        },
-        zoom: 12
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    function pos(lat, lng) {
-        return new google.maps.LatLng(lat, lng);
-    }
-
-    function marker(lat, lng) {
-        var latlng = pos(lat, lng);
-        return new google.maps.Marker({
-            position: latlng,
-            map: map,
-            title: "Hello World!",
-            animation: google.maps.Animation.DROP
-        });
-    }
-
-    var TOKEN = 'token';
-    var OFFERS = 'offers';
-
-    var cfg = {};
-
-    cfg.foxUrl = 'http://foxtest.herokuapp.com/v1/';
-    cfg.urls = [TOKEN, OFFERS].reduce(function (obj, operation) {
-        obj[operation] = cfg.foxUrl + operation;
-        return obj;
-    }, {});
-
-    function foxjax(operation, data, callback) {
-        var url = cfg.urls[operation];
-        $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
-            success: callback,
-            error: function () {
-                var msg = 'There was an error while invoking\n' + url;
-                console.log(msg);
-                alert(msg + '\n\nSorry for the ugly alert message.');
-            },
-            headers: {
-                'Authorization': 'Basic ' + btoa('locafox:LocaF#xes!')
-            }
-        });
-    }
-
-    var markers = [];
-
-//    function fetchOffers() {
-//        foxjax(TOKEN, {}, function (data) {
-//                cfg.token = data.token;
-//                foxjax(OFFERS, {token: cfg.token}, function (data) {
-//                    var i = 0;
-//
-//                    function getDelay() {
-//                        return Math.round(Math.random() * 50) + 20; // between 20 and 70
-//                    }
-//
-//                    function schedule() {
-//                        if (i >= data.length) {
-//                            return;
-//                        }
-//                        var m = marker(data[i].lat, data[i].long);
-//                        markers.push(m);
-//                        i++;
-//                        setTimeout(schedule, getDelay());
-//                    }
-//
-//                    schedule();
-//                });
-//            }
-//        );
-//    }
-//
-//    fetchOffers();
-
-//    $('#js-more').on('click', function (ev) {
-//        ev.preventDefault();
-//        fetchOffers();
-//    });
-//
-//    $('#js-clear').on('click', function (ev) {
-//        ev.preventDefault();
-//        $.each(markers, function (i, m) {
-//            m.setMap(null);
-//        });
-//        markers = [];
-//    });
-
-});
-
 (function () {
 
     var map;
     var markers = [];
 
+    /**
+     * Takes an array of {lat, long} coordinate objects obtained from the fox server.
+     * For each one, drops a pin on the map.
+     */
     function dropMarkers(markers_) {
 
+        /**
+         * Instantiate marker and drop it on the map.
+         */
         function putMarker(lat, lng) {
             var marker = new google.maps.Marker({
                 position: new google.maps.LatLng(lat, lng),
                 map: map,
-                title: "Hello World!",
                 animation: google.maps.Animation.DROP
             });
             markers.push(marker);
-            //https://code.google.com/p/gmaps-api-issues/issues/detail?id=3608
+            //could not solve flickering of markers: https://code.google.com/p/gmaps-api-issues/issues/detail?id=3608
         }
 
+        /**
+         * Gets a random number between 20 and 70
+         */
         function getDelay() {
-            return Math.round(Math.random() * 100) + 50; // between 20 and 70
+            return Math.round(Math.random() * 50) + 20;
         }
 
         var i = 0;
 
+        /**
+         * Don't drop all the pins at the same time. Instead space them out randomly for a nice effect.
+         */
         (function schedule() {
             if (i >= markers_.length) {
                 return;
@@ -136,9 +49,6 @@ return;
         'foxtest.controllers',
         'foxtest.services'
     ]);
-
-
-
 
     angular.module('foxtest.controllers', []).controller('foxtestCtrl', function ($scope, offersService) {
 
--- public/js/services.js
+++ public/js/services.js
@@ -1,58 +1,16 @@
 (function () {
 
     var services = angular.module('foxtest.services', []);
-    services.factory('offersService', function (foxService, tokenService) {
 
-        //    var offersService = {};
-        //
-        //
-        //
-        //    offersService.getOffers = function () {
-        //        return $http.post(
-        //            'http://foxtest.herokuapp.com/v1/offers', $.param({ // guess jQuery is still needed after all...
-        //                token: 'mS5ZCiX6zHHRNeZf30FB1VNSNX1Rp37gn80y5DGQ'
-        //            }), {
-        //                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-        //            });
-        //    };
-        //
-        //    return offersService;
 
-        return {
-            getOffers: function (callback) {
-                tokenService.getToken(function (token) {
-                    foxService.request('offers', {token: token}, callback);
-                });
-            }
-        };
-
-    });
-
-    services.factory('tokenService', function (foxService) {
-
-        var token;
-
-        function getNewToken(callback) {
-            foxService.request('token', callback);
-        }
-
-        return {
-            getToken: function (callback) {
-                if (angular.isUndefined(token)) {
-                    getNewToken(function (response) {
-                        token = response.token;
-                        callback(token);
-                    });
-                } else {
-                    callback(token);
-                }
-            }
-        };
-
-    });
-
+    /**
+     * Base "class" for services. Takes care of the authentication and sets necessary headers.
+     * The returned service has only a "request" method with arguments:
+     *      String operation, the service to invoke
+     *      optional Object data, will be serialized to form data
+     *      Function callback, will be invoked with the response data
+     */
     services.factory('foxService', function ($http, config) {
-
         var access = config.username + ':' + config.password;
 
         $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa(access);
@@ -63,20 +21,59 @@
                     callback = data;
                     data = undefined;
                 }
-                data = angular.isUndefined(data) ? '' : $.param(data);
-                $http.post(config.urls[operation], //$.param( // guess jQuery is still needed after all...
-                    //   token: 'mS5ZCiX6zHHRNeZf30FB1VNSNX1Rp37gn80y5DGQ'
-                    data
-                //)
-                    , {
+                data = angular.isUndefined(data) ? '' : $.param(data); // if there is data, serialize it (need jQuery for this -_- )
+                $http.post(config.urls[operation], data, {
                     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-                }).success(callback);
+                }).success(callback); // TODO handle possible error
             }
         };
-
     });
 
 
+    /**
+     * This service gets new offers from the server.
+     */
+    services.factory('offersService', function (foxService, tokenService) {
+
+        return {
+            getOffers: function (callback) {
+                tokenService.getToken(function (token) {
+                    foxService.request('offers', {token: token}, callback);
+                });
+            }
+        };
+    });
+
+
+    /**
+     * This service returns an access token obtained from the server. The token is only obtained once and then cached.
+     */
+    services.factory('tokenService', function (foxService) {
+
+        var token;
+
+        function getNewToken(callback) {
+            foxService.request('token', callback);
+        }
+
+        return {
+            getToken: function (callback) {
+                if (angular.isUndefined(token)) { // Only get token if one isn't already available. A little callback hell.
+                    getNewToken(function (response) {
+                        token = response.token;
+                        callback(token);
+                    });
+                } else {
+                    callback(token);
+                }
+            }
+        };
+    });
+
+
+    /**
+     * Application configuration. Not exactly a service.
+     */
     services.factory('config', function () {
         var config = {
             operations: {
@@ -96,7 +93,6 @@
         }, {});
 
         return config;
-
     });
 
 })();
