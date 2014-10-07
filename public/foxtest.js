45045e4276495c3602c3a3a29e6a552a9e982fd5
Bruno Teixeira <bteixeira666@gmail.com>
2014-10-07 17:18:41 +0200

    Implemented button behaviour. Gave them a shadow when they're clicked.

--- public/foxtest.js
+++ public/foxtest.js
@@ -12,12 +12,13 @@ $(function () {
     function pos(lat, lng) {
         return new google.maps.LatLng(lat, lng);
     }
+
     function marker(lat, lng) {
         var latlng = pos(lat, lng);
         return new google.maps.Marker({
             position: latlng,
             map: map,
-            title:"Hello World!",
+            title: "Hello World!",
             animation: google.maps.Animation.DROP
         });
     }
@@ -34,35 +35,64 @@ $(function () {
     }, {});
 
     function foxjax(operation, data, callback) {
+        var url = cfg.urls[operation];
         $.ajax({
-            url: cfg.urls[operation],
+            url: url,
             type: 'POST',
             data: data,
             success: callback,
+            error: function () {
+                var msg = 'There was an error while invoking\n' + url;
+                console.log(msg);
+                alert(msg + '\n\nSorry for the ugly alert message.');
+            },
             headers: {
                 'Authorization': 'Basic ' + btoa('locafox:LocaF#xes!')
             }
         });
     }
 
-    foxjax(TOKEN, {}, function (data) {
-            cfg.token = data.token;
-            foxjax(OFFERS, {token: cfg.token}, function (data) {
-                var i = 0;
-                function getDelay() {
-                    return Math.round(Math.random() * 50) + 20; // between 20 and 70
-                }
-                function schedule() {
-                    if (i >= data.length) {
-                        return;
+    var markers = [];
+
+    function fetchOffers() {
+        foxjax(TOKEN, {}, function (data) {
+                cfg.token = data.token;
+                foxjax(OFFERS, {token: cfg.token}, function (data) {
+                    var i = 0;
+
+                    function getDelay() {
+                        return Math.round(Math.random() * 50) + 20; // between 20 and 70
                     }
-                    marker(data[i].lat, data[i].long);
-                    i++;
-                    setTimeout(schedule, getDelay());
-                }
-                schedule();
-            });
-        }
-    );
+
+                    function schedule() {
+                        if (i >= data.length) {
+                            return;
+                        }
+                        var m = marker(data[i].lat, data[i].long);
+                        markers.push(m);
+                        i++;
+                        setTimeout(schedule, getDelay());
+                    }
+
+                    schedule();
+                });
+            }
+        );
+    }
+
+    fetchOffers();
+
+    $('#js-more').on('click', function (ev) {
+        ev.preventDefault();
+        fetchOffers();
+    });
+
+    $('#js-clear').on('click', function (ev) {
+        ev.preventDefault();
+        $.each(markers, function (i, m) {
+            m.setMap(null);
+        });
+        markers = [];
+    });
 
 });
