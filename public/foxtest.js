d12a6786f645d36bde75606d1f141c31f80cde31
Bruno Teixeira <bteixeira666@gmail.com>
2014-10-07 14:45:54 +0200

    Forgot to add some files before. Added map and offers are now fetched. This is the most simple thing that works. From now on it should be about actually making it awesome.

--- public/foxtest.js
+++ public/foxtest.js
@@ -1,7 +1,58 @@
-google.maps.event.addDomListener(window, 'load', function () {
+$(function () {
+
     var mapOptions = {
-        center: { lat: -34.397, lng: 150.644},
-        zoom: 8
+        center: {
+            lat: 52.51,
+            lng: 13.37
+        },
+        zoom: 12
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    function pos(lat, lng) {
+        return new google.maps.LatLng(lat, lng);
+    }
+    function marker(lat, lng) {
+        var latlng = pos(lat, lng);
+        return new google.maps.Marker({
+            position: latlng,
+            map: map,
+            title:"Hello World!"
+        });
+    }
+
+    var TOKEN = 'token';
+    var OFFERS = 'offers';
+
+    var cfg = {};
+
+    cfg.foxUrl = 'http://foxtest.herokuapp.com/v1/';
+    cfg.urls = [TOKEN, OFFERS].reduce(function (obj, operation) {
+        obj[operation] = cfg.foxUrl + operation;
+        return obj;
+    }, {});
+
+    function foxjax(operation, data, callback) {
+        $.ajax({
+            url: cfg.urls[operation],
+            type: 'POST',
+            data: data,
+            success: callback,
+            headers: {
+                'Authorization': 'Basic ' + btoa('locafox:LocaF#xes!')
+            }
+        });
+    }
+
+    foxjax(TOKEN, {}, function (data) {
+            cfg.token = data.token;
+            foxjax(OFFERS, {token: cfg.token}, function (data) {
+                $.each(data, function(i, offer) {
+                    console.log(offer);
+                    marker(offer.lat, offer.long);
+                });
+            });
+        }
+    );
+
 });
