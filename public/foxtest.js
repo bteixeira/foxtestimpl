c4e45cb13805cb23107fa855ff28d985775320c7
Bruno Teixeira <bteixeira666@gmail.com>
2014-10-07 13:41:00 +0200

    Added a simple map centered on Sydney

--- public/foxtest.js
+++ public/foxtest.js
@@ -1,3 +1,7 @@
-$(function () {
-
+google.maps.event.addDomListener(window, 'load', function () {
+    var mapOptions = {
+        center: { lat: -34.397, lng: 150.644},
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 });
